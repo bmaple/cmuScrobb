f7ff9d30f18576c4335161edfd3035903596823d
benjamin maple <rationalwasp@gmail.com>
2014-05-07 02:45:39 -0400

    cleaned up auth process, havent tested it yet

--- main.py
+++ main.py
@@ -34,33 +34,18 @@ import mechanize
     #make authenticated web service calls
     #sign my calls
 #fix url encodes for url
-def auth():
-    #f = open('auth', 'r')
-    username = ''
-    password = ''
-    #f.close()
-    #print (username + password)
-    api_key = '242fedcf48db479f1584797a4e25d771'
-    rootApi = 'http://ws.audioscrobbler.com/2.0/'
-    auth_url = 'http://www.last.fm/api/auth/'
-    mysecret =  'f8365bed081c880685acad58866b4939'
-    api_sig = ''
-    token = ''
-    tokenParams = {'method': 'auth.gettoken', 'api_key': api_key}
-    userAuthParams = {'api_key': api_key, 'token': token}
-    sessionParams = {'method': 'auth.getSession', 'api_key': api_key, 'api_sig': api_sig, 'token': token}
+def getToken(api_key, rootApi): # needs api key and rootapi url 
     inJson = '&format=json'
-    #api_sig = md5('api_key'+api_key+'method' + 'auth.getSession' +'token')
-    
-    #get token begin
+    tokenParams = {'method': 'auth.gettoken', 'api_key': api_key}
     url = rootApi + urllib.urlencode(tokenParams) + inJson
     tokenReqJson = urllib2.urlopen(url)#gets auth from user
     tokenJsonString = tokenReqJson.read()
     tokenJson = json.loads(tokenJsonString)
-    token = tokenJson['token']
-    #get token end
-    
-    #req user auth begin
+    return tokenJson['token']
+def userAuth(auth_url):
+    username = ''
+    password = ''
+    userAuthParams = {'api_key': api_key, 'token': token}
     url = auth_url + urllib.urlencode(userAuthParams)
     br = mechanize.Browser(factory=mechanize.RobustFactory())
     br.set_handle_robots(False)
@@ -71,27 +56,36 @@ def auth():
     res = br.submit()
     br.select_form(nr=2) 
     res = br.submit()
-    #req user auth end 
-
-    #fetch web service session begin
+def getApiSig(api_key, token, mysecret):
     api_sig_hash = 'api_key' + api_key + 'method' + 'auth.getSession' + 'token'+ token + mysecret
     api_sig_hash = api_sig_hash.encode('utf-8')
     api_sig = hashlib.md5(api_sig_hash)
-    api_sig = api_sig.hexdigest()
-
+    return api_sig.hexdigest()
+def getSession(rootApi, api_key, api_sig, token):
+    inJson = '&format=json'
+    sessionParams = {'method': 'auth.getSession', 'api_key': api_key, 'api_sig': api_sig, 'token': token}
     url = rootApi + urllib.urlencode(sessionParams) + inJson
     sessionReqJson = urllib2.urlopen(url)
     sessionJsonString = sessionReqJson.read()
     sessionJson = json.loads(sessionJsonString)
-    sessionkey = sessionJson['session']['key']
-    #fetch web service session end
+    return sessionJson['session']['key']
 
-
-#class user(self):
-#    username = ''
-#    password = ''
-#    api_key = '242fedcf48db479f1584797a4e25d771'
-
+def auth():
+    #f = open('auth', 'r')
+    #f.close()
+    #print (username + password)
+    api_key = '242fedcf48db479f1584797a4e25d771'
+    rootApi = 'http://ws.audioscrobbler.com/2.0/'
+    auth_url = 'http://www.last.fm/api/auth/'
+    mysecret =  'f8365bed081c880685acad58866b4939'
+    api_sig = ''
+    token = ''
+    sessionKey = ''
+     
+    token = getToken(api_key, rootApi)
+    userAuth(auth_url)
+    api_sig = getApiSig(api_key, token, mysecret)
+    sessionKey = getSession(rootApi, api_key, api_sig, token)
 
 
 #class curPlaying(self):
